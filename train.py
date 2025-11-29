992e2d865ba333dddd7d6fdbb5acdedd25a0a990
shabnamzareshahraki <shabnam.zareshahraki@studenti.unipd.it>
2025-11-29 16:36:15 +0100

    something done!

--- train.py
+++ train.py
@@ -39,3 +39,37 @@ def test(model, data):
         accuracies.append(acc)
 
     return accuracies
+
+
+def main():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    dataset = load_dataset()
+    data = dataset[0].to(device)
+
+    model = GAT(
+        in_dim=dataset.num_node_features,
+        hidden_dim=8,
+        out_dim=dataset.num_classes,
+        heads=8
+    ).to(device)
+
+    optimizer = torch.optim.Adam(
+        model.parameters(),
+        lr=0.005,
+        weight_decay=5e-4
+    )
+
+    print('Training GAT on Cora...')
+    for epoch in range(1, 201):
+        loss = train(model, data, optimizer)
+        train_acc, val_acc, test_acc = test(model, data)
+
+        print(f'Epoch: {epoch:03d} | \n'
+              f'Loss: {loss:.4f} | \n'
+              f'Val Accuracy: {val_acc:.4f} | \n'
+              f'Test Accuracy: {test_acc:.4f}')
+
+
+if __name__=='__main__':
+    main()
