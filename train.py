9d57f77edfa08e6ac0ba3678bb48091dc4a9ec69
shabnamzareshahraki <shabnam.zareshahraki@studenti.unipd.it>
2025-11-29 16:28:32 +0100

    something done!

--- train.py
+++ train.py
@@ -13,3 +13,15 @@ def load_dataset():
     )
 
     return dataset
+
+def train(model, data, optimizer):
+    model.train()
+    optimizer.zero_grad()
+
+    out = model(data.x, data.edge_index)
+
+    loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+    loss.backward()
+    optimizer.step()
+
+    return loss.item()
